936e00d8c1dced0722136c771f09760770168d29
Clemens Sauer <clemens.sauer@gmx.net>
2019-10-07 13:24:03 +0200

    json finished

---
+++ Challenge/grocery_store_script.js
@@ -0,0 +1,85 @@
+var number_of_articles = 6
+var articles = [{
+
+   "article_id": "1",
+
+   "description": "Banana",
+
+   "type_of_food" : "fruit",
+
+   "price" : "1€",
+
+   "link_online" : "https://cdn.pixabay.com/photo/2014/12/21/23/39/bananas-575773_960_720.png",
+
+   "link": "https://www.billa.at/produkte/ja-natuerlich-bananen/00-402489",
+
+}, {
+
+   "article_id": "2",
+
+   "description": "Apple",
+
+   "type_of_food" : "fruit",
+
+   "price" : "2€",
+
+   "link_online" : "https://cdn.pixabay.com/photo/2016/11/18/13/47/apple-1834639_960_720.jpg",
+
+   "link": "https://www.billa.at/produkte/pink-lady-apfel-tasse/00-89243",
+
+}, {
+
+    "article_id": "3",
+
+   "description": "Orange",
+
+   "type_of_food" : "fruit",
+
+   "price" : "3€",
+
+   "link_online" : "https://image.shutterstock.com/image-photo/orang-fruit-isolate-orange-leaves-600w-543103645.jpg",
+
+   "link": "https://www.billa.at/produkte/pink-lady-apfel-tasse/00-89243",
+
+}, {
+
+    "article_id": "4",
+
+   "description": "Kiwi",
+
+   "type_of_food" : "fruit",
+
+   "price" : "2€",
+
+   "link_online" : "https://image.shutterstock.com/image-photo/ripe-whole-kiwi-fruit-half-600w-699645964.jpg",
+
+   "link": "https://www.billa.at/produkte/kiwi/00-400018",
+
+}, {
+
+    "article_id": "5",
+
+   "description": "Strawberry",
+
+   "type_of_food" : "fruit",
+
+   "price" : "5€",
+
+   "link_online" : "https://image.shutterstock.com/image-photo/three-strawberries-strawberry-leaf-on-600w-771314446.jpg",
+
+   "link": "https://www.billa.at/produkte/da-komm-ich-her-erdbeeren/00-89480",
+
+}, {
+
+    "article_id": "6",
+
+   "description": "Blueberry",
+
+   "type_of_food" : "fruit",
+
+   "price" : "8€",
+
+   "link_online" : "https://image.shutterstock.com/image-photo/blueberry-isolated-on-white-background-260nw-1500487817.jpg",
+
+   "link": "https://www.billa.at/produkte/heidelbeeren/00-410607",
+}];
